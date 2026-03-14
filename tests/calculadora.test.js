87cb6df7c4c28b926d84d21a1d2ee21ded208c01
Jose Rangel <rangelreyesjose.jarr@gmail.com>
2026-03-14 15:25:54 -0700

    Fix lint issues

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -2,5 +2,5 @@ import { expect, test } from 'vitest';
 import { sumar } from '../src/calculadora';
 
 test('Debe sumar 2 + 3 y retornar 5', () => {
-    expect(sumar(2, 3)).toBe(5);
+  expect(sumar(2, 3)).toBe(5);
 });
