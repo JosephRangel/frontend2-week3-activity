a95db91c46cff7328b3efe9d6ea63ec37b374a71
Daniel Contreras <dcg.sspm@gmail.com>
2026-03-20 21:48:52 -0700

    Add restar function and unit test

--- src/calculadora.js
+++ src/calculadora.js
@@ -2,3 +2,7 @@
 export function sumar(a, b) {
   return a + b;
 }
+
+export function restar(a, b) {
+  return a - b;
+}
--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,6 +1,10 @@
 import { expect, test } from 'vitest';
-import { sumar } from '../src/calculadora';
+import { sumar, restar } from '../src/calculadora';
 
 test('Debe sumar 2 + 3 y retornar 5', () => {
   expect(sumar(2, 3)).toBe(5);
+});
+
+test('Debe restar 5 - 3 y retornar 2', () => {
+  expect(restar(5, 3)).toBe(2);
 });
